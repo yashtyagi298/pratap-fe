dd0e492bd7d2196a86664ceb291ce306350492b5
yashtyagi298 <tyagiyash097@gmail.com>
2026-03-20 16:41:50 +0530

    resolve error

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { Contact } from "lucide-react";
+
 import About from "./components/About";
 
 import Footer from "./components/Footer";
 import Hero from "./components/hero";
 import Marquee from "./components/Marquee";
 import Navbar from "./components/navbar";
-import Pricing from "./components/pricing";
+
 import QuickEnquiry from "./components/QuickEnquiry";
-import Services from "./components/services";
+
 import Stats from "./components/Stats";
 import Ticker from "./components/Ticker";
 import WhyChoose from "./components/Whychoose";
 import Contact1 from "./components/Contact1";
+import Services from "./components/services";
 
 
 export default function App() {
