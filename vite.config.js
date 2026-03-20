83e5c3353878543539f2ab0b283d825ecc17e34d
yashtyagi298 <tyagiyash097@gmail.com>
2026-03-20 17:20:08 +0530

    error resolve

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    react(),       // ✅ VERY IMPORTANT
     tailwindcss(),
   ],
-  base:"/"
-})
+  base: "/",       // ✅ correct
+});
